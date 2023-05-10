c6e68525f33cab4eebb6df84815e24e8f55308a2
Robbendebiene <robbendebiene@mailbox.org>
2023-05-10 08:40:24 +0200

    Restructure files

---
+++ lib/base.dart
@@ -0,0 +1,7 @@
+library flutter_mvvm_architecture;
+
+export 'src/base/repository.dart';
+export 'src/base/service.dart';
+export 'src/base/view.dart';
+export 'src/base/view_model.dart';
+export 'src/base/record.dart';
---
+++ lib/extras.dart
@@ -0,0 +1,4 @@
+library flutter_mvvm_architecture;
+
+export 'src/utils/user_prompt.dart';
+export 'src/utils/user_notification.dart';
