e38548212346950204afab76af8117a3a1d8fb20
Robbendebiene <robbendebiene@mailbox.org>
2023-05-15 15:06:36 +0200

    Add NotificationHandler alternative

--- lib/src/utils/user_notification.dart
+++ lib/src/utils/user_notification.dart
@@ -51,7 +51,7 @@ mixin NotificationMediator on ViewModel {
   final _streamController = StreamController<Notification>();
   late final _notificationRequests = ObservableStream(_streamController.stream);
 
-  /// Requests input from the view
+  /// Requests the view to display a notification.
 
   void notifyUser(String message, {String actionLabel = '', VoidCallback? action, VoidCallback? onVisible, SnackbarCloseCallback? onClosed}) {
     assert(
@@ -85,6 +85,8 @@ mixin NotificationMediator on ViewModel {
 ///
 /// **Note:** This requires a [ScaffoldMessenger] and [Scaffold] above this [View].
 ///
+/// If your [View] itself provides the [Scaffold] you can alternatively insert and use the [NotificationBuilder] below the [Scaffold].
+///
 /// Example usage:
 /// ```dart
 /// class MyViewModel extends ViewModel with NotificationMediator {
@@ -100,19 +102,12 @@ mixin NotificationMediator on ViewModel {
 
 mixin NotificationHandler<T extends NotificationMediator> on View<T> {
 
-  /// Override this to build a custom prompt widget dialog.
+  /// Override this to build a custom notification snackbar.
+  ///
+  /// Otherwise this will use the [defaultNotificationBuilder].
 
   SnackBar notificationBuilder(BuildContext context, Notification request) {
-    return SnackBar(
-      content: Text(request.message),
-      onVisible: request.onVisible,
-      action: request.action != null
-        ? SnackBarAction(
-          label: request.actionLabel,
-          onPressed: request.action!,
-        )
-        : null,
-    );
+    return defaultNotificationBuilder(context, request);
   }
 
   @override
@@ -131,4 +126,82 @@ mixin NotificationHandler<T extends NotificationMediator> on View<T> {
       }
     });
   }
+}
+
+
+typedef NotificationSnackBarBuilder = SnackBar Function(BuildContext context, Notification request);
+
+/// An alternative way to show notifications provided by the [NotificationMediator].
+///
+/// This must be placed below a [ScaffoldMessenger] and [Scaffold] in order to work.
+///
+/// ATTENTION: You have to provide the view models type as [T] that provides the notifications.
+
+class NotificationBuilder<T extends NotificationMediator> extends StatefulWidget {
+  final Widget child;
+  final NotificationSnackBarBuilder builder;
+
+  const NotificationBuilder({
+    required this.child,
+    this.builder = defaultNotificationBuilder,
+    super.key,
+  });
+
+  @override
+  NotificationBuilderState createState() => NotificationBuilderState<T>();
+}
+
+class NotificationBuilderState<T extends NotificationMediator> extends State<NotificationBuilder> {
+  ReactionDisposer? _disposeReaction;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    final result = context.dependOnInheritedWidgetOfExactType<ViewModelProvider<T>>();
+    assert(result != null, 'The NotificationBuilder "$widget" cannot find "$T" in the current context.');
+    final viewModel = result!.viewModel;
+
+    _disposeReaction?.call();
+    _disposeReaction = _reactionBuilder(viewModel);
+  }
+
+  @override
+  Widget build(BuildContext context) => widget.child;
+
+  @override
+  void dispose() {
+    _disposeReaction?.call();
+    super.dispose();
+  }
+
+  ReactionDisposer _reactionBuilder(T viewModel) {
+    return reaction((_) => viewModel._notificationRequests.value, (result) {
+      if (result != null) {
+        final messenger = ScaffoldMessenger.of(context);
+        final controller = messenger.showSnackBar(
+          widget.builder(context, result),
+        );
+        if (result.onClosed != null) {
+          controller.closed.then(result.onClosed!);
+        }
+      }
+    });
+  }
+}
+
+
+/// The default builder for notifications dispatched by the [NotificationMediator].
+
+SnackBar defaultNotificationBuilder(BuildContext context, Notification request) {
+  return SnackBar(
+    content: Text(request.message),
+    onVisible: request.onVisible,
+    action: request.action != null
+      ? SnackBarAction(
+        label: request.actionLabel,
+        onPressed: request.action!,
+      )
+      : null,
+  );
 }
